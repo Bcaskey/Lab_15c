2e987f3916ea39d0200b5acbedace692db234599
Bcaskey <bcaskeyu@charter.net>
2016-03-22 18:16:57 -0500

    Completed damage function on click event

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var Vehicle = (function () {
         var _this = this;
         this.div = document.createElement('div');
         this.div.className = 'myVehicle';
-        this.div.id = '5'; //will have to parse later 
+        this.div.id = '5';
         this.div.addEventListener('click', function () {
             _this.delete();
         });
@@ -39,11 +39,8 @@ var Vehicle = (function () {
         setTimeout(goRight, 50);
     };
     Vehicle.prototype.delete = function () {
-        console.log('test');
         var damage = this.div.id;
         var x = +damage;
-        // damage = parseInt(damage, 10);
-        // let x = this.div.id;
         var y = x - 1;
         this.div.innerHTML = "" + y;
         if (x > 1) {
@@ -53,11 +50,252 @@ var Vehicle = (function () {
         else {
             this.div.remove();
         }
-        // this.div.onclick = function(e){
-        // }
     };
     return Vehicle;
 }());
+// BELOW CAR  BELOW CAR BELOW CAR  ---------------------------
+var Car = (function () {
+    function Car() {
+        var _this = this;
+        this.div = document.createElement('div');
+        this.div.className = 'myCar';
+        this.div.id = '2';
+        this.div.addEventListener('click', function () {
+            _this.delete();
+        });
+    }
+    Car.prototype.insert = function () {
+        var myGameArea = document.getElementById('gameArea');
+        myGameArea.appendChild(this.div);
+        this.div.style.position = 'absolute';
+        var boxText = this.div.id;
+        this.div.innerHTML = boxText;
+        var ww = window.innerWidth;
+        var wh = window.innerHeight;
+        this.div.style.left = Math.floor(Math.random() * (ww - 140)) + 'px';
+        this.div.style.top = Math.floor(Math.random() * (wh - 100)) + 'px';
+    };
+    Car.prototype.move = function () {
+        var ww = window.innerWidth;
+        function goRight() {
+            $(".myCar").animate({
+                left: ww
+            }, 5000, function () {
+                setTimeout(goLeft, 50);
+            });
+        }
+        function goLeft() {
+            $(".myCar").animate({
+                left: 0
+            }, 5000, function () {
+                setTimeout(goRight, 50);
+            });
+        }
+        setTimeout(goRight, 50);
+    };
+    Car.prototype.delete = function () {
+        var damage = this.div.id;
+        var x = +damage;
+        var y = x - 1;
+        this.div.innerHTML = "" + y;
+        if (x > 1) {
+            var newID = x - 1;
+            this.div.id = "" + newID;
+        }
+        else {
+            this.div.remove();
+        }
+    };
+    return Car;
+}());
+function addCar() {
+    var car = new Car();
+    car.insert();
+    vehicleArray.push(car);
+}
+// BELOW COPCAR  BELOW COPCAR BELOW COPCAR  ---------------------------
+var CopCar = (function () {
+    function CopCar() {
+        var _this = this;
+        this.div = document.createElement('div');
+        this.div.className = 'myCopCar';
+        this.div.id = '3';
+        this.div.addEventListener('click', function () {
+            _this.delete();
+        });
+    }
+    CopCar.prototype.insert = function () {
+        var myGameArea = document.getElementById('gameArea');
+        myGameArea.appendChild(this.div);
+        this.div.style.position = 'absolute';
+        var boxText = this.div.id;
+        this.div.innerHTML = boxText;
+        var ww = window.innerWidth;
+        var wh = window.innerHeight;
+        this.div.style.left = Math.floor(Math.random() * (ww - 140)) + 'px';
+        this.div.style.top = Math.floor(Math.random() * (wh - 100)) + 'px';
+    };
+    CopCar.prototype.move = function () {
+        var wh = window.innerHeight;
+        function goDown() {
+            $(".myCopCar").animate({
+                top: wh
+            }, 5000, function () {
+                setTimeout(goUp, 50);
+            });
+        }
+        function goUp() {
+            $(".myCopCar").animate({
+                top: 0
+            }, 10, function () {
+                setTimeout(goDown, 50);
+            });
+        }
+        setTimeout(goDown, 50);
+    };
+    CopCar.prototype.delete = function () {
+        var damage = this.div.id;
+        var x = +damage;
+        var y = x - 1;
+        this.div.innerHTML = "" + y;
+        if (x > 1) {
+            var newID = x - 1;
+            this.div.id = "" + newID;
+        }
+        else {
+            this.div.remove();
+        }
+    };
+    return CopCar;
+}());
+function addCopCar() {
+    var copCar = new CopCar();
+    copCar.insert();
+    vehicleArray.push(copCar);
+}
+// BELOW MOTORCYCLE  BELOW MOTORCYCLE BELOW MOTORCYCLE  ---------------------------
+var Motorcycle = (function () {
+    function Motorcycle() {
+        var _this = this;
+        this.div = document.createElement('div');
+        this.div.className = 'myMotorcycle';
+        this.div.id = '1';
+        this.div.addEventListener('click', function () {
+            _this.delete();
+        });
+    }
+    Motorcycle.prototype.insert = function () {
+        var myGameArea = document.getElementById('gameArea');
+        myGameArea.appendChild(this.div);
+        this.div.style.position = 'absolute';
+        var boxText = this.div.id;
+        this.div.innerHTML = boxText;
+        var ww = window.innerWidth;
+        var wh = window.innerHeight;
+        this.div.style.left = Math.floor(Math.random() * (ww - 140)) + 'px';
+        this.div.style.top = Math.floor(Math.random() * (wh - 100)) + 'px';
+    };
+    Motorcycle.prototype.move = function () {
+        var ww = window.innerWidth;
+        var wh = window.innerHeight;
+        function goRight() {
+            $(".myMotorcycle").animate({
+                left: ww,
+                top: wh
+            }, 2000, function () {
+                setTimeout(goLeft, 50);
+            });
+        }
+        function goLeft() {
+            $(".myMotorcycle").animate({
+                left: 0,
+                top: 0
+            }, 10, function () {
+                setTimeout(goRight, 50);
+            });
+        }
+        setTimeout(goRight, 50);
+    };
+    Motorcycle.prototype.delete = function () {
+        var damage = this.div.id;
+        var x = +damage;
+        var y = x - 1;
+        this.div.innerHTML = "" + y;
+        if (x > 1) {
+            var newID = x - 1;
+            this.div.id = "" + newID;
+        }
+        else {
+            this.div.remove();
+        }
+    };
+    return Motorcycle;
+}());
+function addMotorcycle() {
+    var motorcycle = new Motorcycle();
+    motorcycle.insert();
+    vehicleArray.push(motorcycle);
+}
+// BELOW TANK  BELOW TANK BELOW TANK  ----------------------------------------
+var Tank = (function () {
+    function Tank() {
+        var _this = this;
+        this.div = document.createElement('div');
+        this.div.className = 'myTank';
+        this.div.id = '10';
+        this.div.addEventListener('click', function () {
+            _this.delete();
+        });
+    }
+    Tank.prototype.insert = function () {
+        var myGameArea = document.getElementById('gameArea');
+        myGameArea.appendChild(this.div);
+        this.div.style.position = 'absolute';
+        var boxText = this.div.id;
+        this.div.innerHTML = boxText;
+        var ww = window.innerWidth;
+        var wh = window.innerHeight;
+        this.div.style.left = Math.floor(Math.random() * (ww - 140)) + 'px';
+        this.div.style.top = Math.floor(Math.random() * (wh - 100)) + 'px';
+    };
+    Tank.prototype.move = function () {
+        var wh = window.innerHeight;
+        function goUp() {
+            $(".myTank").animate({
+                top: 0
+            }, 10000, function () {
+                setTimeout(goDown, 50);
+            });
+        }
+        function goDown() {
+            $(".myTank").animate({
+                top: wh
+            }, 10, function () {
+                setTimeout(goUp, 50);
+            });
+        }
+        setTimeout(goUp, 50);
+    };
+    Tank.prototype.delete = function () {
+        var damage = this.div.id;
+        var x = +damage;
+        var y = x - 1;
+        this.div.innerHTML = "" + y;
+        if (x > 1) {
+            var newID = x - 1;
+            this.div.id = "" + newID;
+        }
+        else {
+            this.div.remove();
+        }
+    };
+    return Tank;
+}());
+function addTank() {
+    var tank = new Tank();
+    tank.insert();
+    vehicleArray.push(tank);
+}
 function addVehicle() {
     var vehicle = new Vehicle();
     vehicle.insert();
--- script.ts
+++ script.ts
@@ -6,7 +6,7 @@ class Vehicle {
     constructor() {
      this.div = document.createElement('div');
      this.div.className = 'myVehicle';
-     this.div.id = '5'; //will have to parse later 
+     this.div.id = '5';
      this.div.addEventListener('click', () => {
          this.delete();
      });
@@ -41,25 +41,264 @@ class Vehicle {
         setTimeout(goRight, 50);
     }
     delete() {
-            console.log('test');
             let damage = this.div.id;
             let x = +damage;
-            // damage = parseInt(damage, 10);
-            // let x = this.div.id;
             let y = x - 1;
             this.div.innerHTML = ""+y;
             if (x > 1) {
                 let newID = x - 1;
                 this.div.id = ""+newID;
-                //console.log(newID);
             } else {
              this.div.remove();
-             //console.log('test');   
             }
-        // this.div.onclick = function(e){
-            
-        // }
     }    
+}
+// BELOW CAR  BELOW CAR BELOW CAR  ---------------------------
+class Car {
+    div: HTMLDivElement;
+    
+    constructor() {
+     this.div = document.createElement('div');
+     this.div.className = 'myCar';
+     this.div.id = '2';
+     this.div.addEventListener('click', () => {
+         this.delete();
+     });
+    }
+    insert() {
+        let myGameArea = document.getElementById('gameArea');
+        myGameArea.appendChild(this.div);
+        this.div.style.position = 'absolute';
+        let boxText = this.div.id;
+        this.div.innerHTML = boxText;
+        let ww = window.innerWidth;
+        let wh = window.innerHeight;
+        this.div.style.left = Math.floor(Math.random() * (ww - 140)) + 'px';
+        this.div.style.top = Math.floor(Math.random() * (wh - 100)) + 'px';
+    }
+    move() {
+        let ww = window.innerWidth;
+        function goRight() {
+            $(".myCar").animate({
+            left: ww
+        }, 5000, function() {
+            setTimeout(goLeft, 50);
+        });
+        }
+        function goLeft() {
+            $(".myCar").animate({
+            left: 0
+        }, 5000, function() {
+            setTimeout(goRight, 50);
+        });
+        }
+        setTimeout(goRight, 50);
+    }
+    delete() {
+            let damage = this.div.id;
+            let x = +damage;
+            let y = x - 1;
+            this.div.innerHTML = ""+y;
+            if (x > 1) {
+                let newID = x - 1;
+                this.div.id = ""+newID;
+            } else {
+             this.div.remove();
+            }
+    }    
+}
+
+function addCar() { 
+    let car = new Car();
+    car.insert();
+    vehicleArray.push(car);
+}
+
+// BELOW COPCAR  BELOW COPCAR BELOW COPCAR  ---------------------------
+class CopCar {
+    div: HTMLDivElement;
+    
+    constructor() {
+     this.div = document.createElement('div');
+     this.div.className = 'myCopCar';
+     this.div.id = '3';
+     this.div.addEventListener('click', () => {
+         this.delete();
+     });
+    }
+    insert() {
+        let myGameArea = document.getElementById('gameArea');
+        myGameArea.appendChild(this.div);
+        this.div.style.position = 'absolute';
+        let boxText = this.div.id;
+        this.div.innerHTML = boxText;
+        let ww = window.innerWidth;
+        let wh = window.innerHeight;
+        this.div.style.left = Math.floor(Math.random() * (ww - 140)) + 'px';
+        this.div.style.top = Math.floor(Math.random() * (wh - 100)) + 'px';
+    }
+    move() {
+        let wh = window.innerHeight;
+        function goDown() {
+            $(".myCopCar").animate({
+            top: wh
+        }, 5000, function() {
+            setTimeout(goUp, 50);
+        });
+        }
+        function goUp() {
+            $(".myCopCar").animate({
+            top: 0
+        }, 10, function() {
+            setTimeout(goDown, 50);
+        });
+        }
+        setTimeout(goDown, 50);
+    }
+    delete() {
+            let damage = this.div.id;
+            let x = +damage;
+            let y = x - 1;
+            this.div.innerHTML = ""+y;
+            if (x > 1) {
+                let newID = x - 1;
+                this.div.id = ""+newID;
+            } else {
+             this.div.remove();
+            }
+    }    
+}
+
+function addCopCar() { 
+    let copCar = new CopCar();
+    copCar.insert();
+    vehicleArray.push(copCar);
+}
+
+// BELOW MOTORCYCLE  BELOW MOTORCYCLE BELOW MOTORCYCLE  ---------------------------
+class Motorcycle {
+    div: HTMLDivElement;
+    
+    constructor() {
+     this.div = document.createElement('div');
+     this.div.className = 'myMotorcycle';
+     this.div.id = '1';
+     this.div.addEventListener('click', () => {
+         this.delete();
+     });
+    }
+    insert() {
+        let myGameArea = document.getElementById('gameArea');
+        myGameArea.appendChild(this.div);
+        this.div.style.position = 'absolute';
+        let boxText = this.div.id;
+        this.div.innerHTML = boxText;
+        let ww = window.innerWidth;
+        let wh = window.innerHeight;
+        this.div.style.left = Math.floor(Math.random() * (ww - 140)) + 'px';
+        this.div.style.top = Math.floor(Math.random() * (wh - 100)) + 'px';
+    }
+    move() {
+        let ww = window.innerWidth;
+        let wh = window.innerHeight;
+        function goRight() {
+            $(".myMotorcycle").animate({
+            left: ww,
+            top: wh
+        }, 2000, function() {
+            setTimeout(goLeft, 50);
+        });
+        }
+        function goLeft() {
+            $(".myMotorcycle").animate({
+            left: 0,
+            top: 0
+        }, 10, function() {
+            setTimeout(goRight, 50);
+        });
+        }
+        setTimeout(goRight, 50);
+    }
+    delete() {
+            let damage = this.div.id;
+            let x = +damage;
+            let y = x - 1;
+            this.div.innerHTML = ""+y;
+            if (x > 1) {
+                let newID = x - 1;
+                this.div.id = ""+newID;
+            } else {
+             this.div.remove();
+            }
+    }    
+}
+
+function addMotorcycle() { 
+    let motorcycle = new Motorcycle();
+    motorcycle.insert();
+    vehicleArray.push(motorcycle);
+}
+
+// BELOW TANK  BELOW TANK BELOW TANK  ----------------------------------------
+
+class Tank {
+    div: HTMLDivElement;
+    
+    constructor() {
+     this.div = document.createElement('div');
+     this.div.className = 'myTank';
+     this.div.id = '10';
+     this.div.addEventListener('click', () => {
+         this.delete();
+     });
+    }
+    insert() {
+        let myGameArea = document.getElementById('gameArea');
+        myGameArea.appendChild(this.div);
+        this.div.style.position = 'absolute';
+        let boxText = this.div.id;
+        this.div.innerHTML = boxText;
+        let ww = window.innerWidth;
+        let wh = window.innerHeight;
+        this.div.style.left = Math.floor(Math.random() * (ww - 140)) + 'px';
+        this.div.style.top = Math.floor(Math.random() * (wh - 100)) + 'px';
+    }
+    move() {
+        let wh = window.innerHeight;
+        function goUp() {
+            $(".myTank").animate({
+            top: 0
+        }, 10000, function() {
+            setTimeout(goDown, 50);
+        });
+        }
+        function goDown() {
+            $(".myTank").animate({
+            top: wh
+        }, 10, function() {
+            setTimeout(goUp, 50);
+        });
+        }
+        setTimeout(goUp, 50);
+    }
+    delete() {
+            let damage = this.div.id;
+            let x = +damage;
+            let y = x - 1;
+            this.div.innerHTML = ""+y;
+            if (x > 1) {
+                let newID = x - 1;
+                this.div.id = ""+newID;
+            } else {
+             this.div.remove();
+            }
+    }    
+}
+
+function addTank() { 
+    let tank = new Tank();
+    tank.insert();
+    vehicleArray.push(tank);
 }
 
 function addVehicle() { 
